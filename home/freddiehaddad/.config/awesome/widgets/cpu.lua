e158963b4cd7703305ab7a12ec7d6336b3979d11
Freddie Haddad <freddie.haddad@outlook.com>
2023-12-15 11:41:56 -0800

    fix: use correct widget path in cpu module

--- home/freddiehaddad/.config/awesome/widgets/cpu.lua
+++ home/freddiehaddad/.config/awesome/widgets/cpu.lua
@@ -21,7 +21,7 @@ local CMD = [[sh -c "grep '^cpu.' /proc/stat; ps -eo 'pid:10,pcpu:5,pmem:5,comm:
 local CMD_slim = [[grep --max-count=1 '^cpu.' /proc/stat]]
 
 local HOME_DIR = os.getenv("HOME")
-local WIDGET_DIR = HOME_DIR .. '/.config/awesome/awesome-wm-widgets/cpu-widget'
+local WIDGET_DIR = HOME_DIR .. '/.config/awesome/widgets/cpu-widget'
 
 local cpu_widget = {}
 local cpu_rows = {
