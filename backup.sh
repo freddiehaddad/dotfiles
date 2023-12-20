386e2b19efe03888bdedbf4f985173898d494ba1
Freddie Haddad <freddie.haddad@outlook.com>
2023-12-19 19:06:34 -0800

    easyeffects: add backup rule

--- backup.sh
+++ backup.sh
@@ -3,7 +3,7 @@
 OPTIONS="--verbose --recursive --relative"
 
 rsync $OPTIONS "$HOME"/.zshrc .
-rsync $OPTIONS "$HOME"/.config/{alacritty,awesome,btop,dircolors,foot,hypr,picom,starship.toml,sway,tmux/tmux.conf,waybar,wofi} .
+rsync $OPTIONS "$HOME"/.config/{alacritty,awesome,btop,dircolors,easyeffects,foot,hypr,picom,starship.toml,sway,tmux/tmux.conf,waybar,wofi} .
 rsync $OPTIONS /boot/loader/{entries,loader.conf} .
 rsync $OPTIONS /etc/{makepkg.conf,mkinitcpio.conf,modules-load.d/openrgb.conf,nsswitch.conf,pacman.conf,skel/.zshrc,systemd/network/20-ethernet.network,systemd/resolved.conf.d/20-multicastdns.conf,xdg/reflector/reflector.conf,xdg/user-dirs.conf,xdg/user-dirs.defaults,X11/xorg.conf.d/20-amdgpu.conf} .
 
