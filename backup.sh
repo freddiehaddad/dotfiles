c0031dc06ba4f9e7ac3a82e7feb45e82b2e5bd17
Freddie Haddad <freddie.haddad@outlook.com>
2024-03-09 09:57:52 -0800

    mkinitcpio: v38 moved microcode hooks to upstream

--- backup.sh
+++ backup.sh
@@ -5,7 +5,6 @@ OPTIONS="--verbose --recursive --relative"
 rsync $OPTIONS "$HOME"/.zshrc .
 rsync $OPTIONS "$HOME"/.drirc .
 rsync $OPTIONS "$HOME"/.config/{alacritty,awesome,btop/btop.conf,dircolors,easyeffects,foot,hypr,picom,starship.toml,sway,tmux/tmux.conf,waybar,wofi} .
-rsync $OPTIONS /boot/loader/{entries,loader.conf} .
 rsync $OPTIONS /etc/{makepkg.conf,mkinitcpio.conf,modules-load.d/openrgb.conf,nsswitch.conf,pacman.conf,skel/.zshrc,systemd/network/20-ethernet.network,systemd/resolved.conf.d/20-multicastdns.conf,xdg/reflector/reflector.conf,xdg/user-dirs.conf,xdg/user-dirs.defaults,X11/xorg.conf.d/20-amdgpu.conf} .
 
 # Requires sudo
@@ -14,3 +13,4 @@ sudo rsync $OPTIONS /etc/{default/useradd,sudoers.d/20-users} .
 sudo chown --recursive freddiehaddad:freddiehaddad ./etc
 sudo rsync $OPTIONS /root/{.zshrc,.config/tmux} .
 sudo chown --recursive freddiehaddad:freddiehaddad ./root
+sudo rsync $OPTIONS /boot/loader/{entries,loader.conf} .
